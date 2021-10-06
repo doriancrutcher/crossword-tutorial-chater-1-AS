d1abf0850c33e3e8c6c470d4db42c885f0cce427
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2021-10-06 14:39:29 -0700

    uploading updated code translated from rust

---
+++ contract/assembly/index.ts
@@ -0,0 +1,48 @@
+// @nearfile out
+import { Context, logging, storage, env, util, math } from "near-sdk-as";
+
+@nearBindgen
+export class Contract {
+  solution: string;
+  constructor(solution: string) {
+    this.solution = solution;
+  }
+
+  getSolution(): string {
+    return this.solution;
+  }
+
+  guess_solution(solution: string): void {
+    logging.log("bytes are");
+    let hashed_input = math.sha256(util.stringToBytes(solution));
+
+    let output: string[] = [];
+    let hex_hashed_input: string = "";
+
+    for (let i = 0; i < solution.length; i++) {
+      let val: u16 = hashed_input[i];
+      let stringVal = val.toString();
+      let outputWord: string = "";
+      for (let x = 0; i < solution.length; i++) {
+        outputWord = outputWord + solution.charCodeAt(i).toString(16);
+      }
+      output[i] = outputWord;
+    }
+    hex_hashed_input = output.join("");
+
+    logging.log(hex_hashed_input);
+    if (
+      hex_hashed_input == storage.get<String>("crosswordSoluton", "nothing")
+    ) {
+      logging.log("You guessed right!");
+    } else {
+      ("try again");
+    }
+  }
+
+  // testReturn():void{
+  //   let x= storage.get<string>('crosswordSolution','no val');
+  //   logging.log('val is')
+  //   logging.log(x)
+  //  }
+}
